d2cc1b4b08367f37d887cec7eb7821256b5615d2
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2025-09-08 18:11:26 +0100

    refactor(install): move common code to base install script

--- install.sh
+++ install.sh
@@ -2,25 +2,7 @@
 
 set -e
 
-[ -z $TMPDIR ] && [ -d /tmp ] && TMPDIR="/tmp"
-
-export PATH="$HOME/.local/bin:$PATH"
-
-echo "Creating directories"
-mkdir -p $HOME/.local/bin
-mkdir -p $HOME/.local/etc
-mkdir -p $HOME/.local/share
-
-echo "Installing piu"
-curl -sfSL https://go.ponces.dev/piu | bash
-
-echo "Installing required packages"
-export DEBIAN_FRONTEND=noninteractive
-piu install -y bash binutils coreutils curl ccze dos2unix file git jq less make nano sudo tar tree unzip wget zip zsh
-
-if [ ! -z "$TERMUX_VERSION" ]; then
-    piu install -y which
-fi
+curl -sfSL https://go.ponces.dev/base | bash
 
 if [ ! -z "$WSL_DISTRO_NAME" ]; then
     piu install -y wslu xdg-utils
@@ -31,30 +13,22 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	curl -sfSL https://go.ponces.dev/zsh | bash
 fi
 
-echo "Installing zsh plugins"
-if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
-	git clone -q --depth=1 https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-fi
-if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
-	git clone -q --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-fi
-
 echo "Installing mise"
 if ! command -v mise >/dev/null; then
     curl -sfSL https://go.ponces.dev/mise | bash
 fi
 
-echo "Installing bitwarden CLI"
+echo "Installing Bitwarden CLI"
 if ! command -v rbw >/dev/null; then
     curl -sfSL https://go.ponces.dev/rbw | bash
 fi
 
-echo "Installing github CLI"
+echo "Installing GitHub CLI"
 if ! command -v gh >/dev/null; then
     curl -sfSL https://go.ponces.dev/github | bash
 fi
 
-echo "Installing azure CLI"
+echo "Installing Azure CLI"
 if ! command -v az >/dev/null; then
     curl -sfSL https://go.ponces.dev/azure | bash
 fi
@@ -64,12 +38,6 @@ if ! command -v chezmoi >/dev/null; then
     curl -sfSL https://go.ponces.dev/chezmoi | bash
 fi
 
-# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-
 set -- init --apply --source="${script_dir}"
-
-echo "Running 'chezmoi $*'" >&2
-
-# Replace current process with chezmoi
 exec "$chezmoi" "$@"
