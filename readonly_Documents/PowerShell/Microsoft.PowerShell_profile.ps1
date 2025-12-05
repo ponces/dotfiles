e7acaaa3f00d5cbe57ce66fc750e58ef98017ba9
Alberto Ponces <ponces26@gmail.com>
2025-12-05 16:13:22 +0000

    fix(Documents): update PS profiles

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,10 @@
-mise activate pwsh | Out-String | Invoke-Expression
-
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/robbyrussell.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/robbyrussell.omp.json" | Invoke-Expression
 Import-Module "$env:APPDATA/posh-git/src/posh-git.psd1"
 
-function cdown() { cd $env:USERPROFILE\Downloads }
-function cgit() { cd $env:USERPROFILE\Git }
+mise activate pwsh | Out-String | Invoke-Expression
+
+function cdown() { cd $env:USERPROFILE/Downloads }
+function cgit() { cd $env:USERPROFILE/Git }
 function md5sum { Get-FileHash -Path $args[0] -Algorithm MD5 | Select-Object -ExpandProperty Hash }
 function sha1sum { Get-FileHash -Path $args[0] -Algorithm SHA1 | Select-Object -ExpandProperty Hash }
 function sha256sum { Get-FileHash -Path $args[0] -Algorithm SHA256 | Select-Object -ExpandProperty Hash }
