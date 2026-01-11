d9ae6b24c740749f3c9c178610753aad0d94818d
Alberto Ponces <ponces26@gmail.com>
2026-01-11 12:18:27 +0000

    feat(Documents): add force argument to the unzip alias

--- readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -9,4 +9,4 @@ function md5sum { Get-FileHash -Path $args[0] -Algorithm MD5 | Select-Object -Ex
 function sha1sum { Get-FileHash -Path $args[0] -Algorithm SHA1 | Select-Object -ExpandProperty Hash }
 function sha256sum { Get-FileHash -Path $args[0] -Algorithm SHA256 | Select-Object -ExpandProperty Hash }
 function sshcode() { code --remote ssh-remote+root@ubuild01.ponces.dev @Args }
-function unzip() { Expand-Archive -Path $args[0] -DestinationPath $(if ($args[1]) { $args[1] } else { Split-Path $args[0] -Parent }) }
+function unzip() { Expand-Archive -Force -Path $args[0] -DestinationPath $(if ($args[1]) { $args[1] } else { Split-Path $args[0] -Parent }) }
